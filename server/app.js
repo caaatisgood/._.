5cc540f2cae2ad590c92f20aed3d22a8c2de052f
imrobinized <almigod0718@gmail.com>
2017-03-17 02:07:45 +0800

    fuck yeah

--- server/app.js
+++ server/app.js
@@ -17,9 +17,42 @@ const date = new Date().toLocaleDateString().split('-').join('')
 const CHALLENGE_PAGE_ENDPOINT = 'https://tw.voicetube.com/everyday/'
 console.log(CHALLENGE_PAGE_ENDPOINT + date)
 
+// request
+//   .get({
+//     url: CHALLENGE_PAGE_ENDPOINT + date,
+//     gzip: true,
+//     headers: {
+//       'cache-control': 'no-cache',
+//       'Accept': '*/*',
+//       'Accept-Encoding': 'gzip, deflate, br',
+//       'User-Agent': 'yee'
+//     }
+//   })
+//   .on('data', data => {
+//     let line = data.toString()
+//     const startStr = 'core/zhTW/spkChallenges/'
+//     const start = line.indexOf(startStr)
+//     if (start > -1) {
+//       line = line.substring(start, line.length)
+//       const endStr = '/comments'
+//       const end = line.indexOf(endStr)
+//       if (end > -1) {
+//         const id = line.substring(startStr.length, end)
+//         console.log('daily challenge id found ->', id)
+
+//         // POST the comments back!
+//       }
+//     }
+//   })
+//   .on('error', err => {
+//     console.log(err)
+//   })
+
+// Retrieving comments
+let buffers = []
 request
-  .get({
-    url: CHALLENGE_PAGE_ENDPOINT + date,
+  .post({
+    url: 'https://vtapi.voicetube.com/v2/core/zhTW/spkChallenges/1249/comments',
     gzip: true,
     headers: {
       'cache-control': 'no-cache',
@@ -29,19 +62,15 @@ request
     }
   })
   .on('data', data => {
-    let line = data.toString()
-    const startStr = 'core/zhTW/spkChallenges/'
-    const start = line.indexOf(startStr)
-    if (start > -1) {
-      line = line.substring(start, line.length)
-      const endStr = '/comments'
-      const end = line.indexOf(endStr)
-      if (end > -1) {
-        console.log('daily challenge id found ->', line.substring(startStr.length, end))
-        // POST the comments back!
-      }
-    }
+    buffers.push(data.toString())
+  })
+  .on('response', response => {
+    console.log('response!')
   })
   .on('error', err => {
     console.log(err)
+  })
+  .on('end', () => {
+    console.log('end!')
+    console.log('buffers', buffers.join(''))
   })
