a4c1a5c2404926903d5a8f896e5e29ff01d94729
imrobinized <almigod0718@gmail.com>
2017-05-07 00:41:12 +0800

    refine codes

--- server/app.js
+++ server/app.js
@@ -1,58 +1,28 @@
+import restify from 'restify'
+import plugins from 'restify-plugins'
 import request from 'request'
+import rp from 'request-promise'
 
-console.log(`
- 1. Get daily challenge page
-    GET https://tw.voicetube.com/everyday/20170311
+let server = restify.createServer()
+server.use(plugins.acceptParser(server.acceptable))
+server.use(plugins.queryParser())
+server.use(plugins.bodyParser())
 
- 2. Parse to get challenge id
-    some where in "core/zhTW/spkChallenges/CHALLENGE_ID/comments"
+server.get('/echo/:name', (req, res, next) => {
+  res.send({params: req.params, query: req.query})
+  return next()
+})
 
- 3. Get comments by challenge id
-    POST https://vtapi.voicetube.com/v2/core/zhTW/spkChallenges/CHALLENGE_ID/comments
-
- 4. And there you go
-`)
+server.listen(9487, () => {
+  console.log('%s listening at %s', server.name, server.url)
+})
 
 const date = new Date().toLocaleDateString().split('-').join('')
 const CHALLENGE_PAGE_ENDPOINT = 'https://tw.voicetube.com/everyday/'
-console.log(CHALLENGE_PAGE_ENDPOINT + date)
 
-// request
-//   .get({
-//     url: CHALLENGE_PAGE_ENDPOINT + date,
-//     gzip: true,
-//     headers: {
-//       'cache-control': 'no-cache',
-//       'Accept': '*/*',
-//       'Accept-Encoding': 'gzip, deflate, br',
-//       'User-Agent': 'yee'
-//     }
-//   })
-//   .on('data', data => {
-//     let line = data.toString()
-//     const startStr = 'core/zhTW/spkChallenges/'
-//     const start = line.indexOf(startStr)
-//     if (start > -1) {
-//       line = line.substring(start, line.length)
-//       const endStr = '/comments'
-//       const end = line.indexOf(endStr)
-//       if (end > -1) {
-//         const id = line.substring(startStr.length, end)
-//         console.log('daily challenge id found ->', id)
-
-//         // POST the comments back!
-//       }
-//     }
-//   })
-//   .on('error', err => {
-//     console.log(err)
-//   })
-
-// Retrieving comments
-let buffers = []
-request
-  .post({
-    url: 'https://vtapi.voicetube.com/v2/core/zhTW/spkChallenges/1249/comments',
+const getCommentId = () => rp
+  .get({
+    url: CHALLENGE_PAGE_ENDPOINT + date,
     gzip: true,
     headers: {
       'cache-control': 'no-cache',
@@ -61,16 +31,55 @@ request
       'User-Agent': 'yee'
     }
   })
-  .on('data', data => {
-    buffers.push(data.toString())
+  .then(data => {
+    let line = data.toString()
+    const startStr = 'core/zhTW/spkChallenges/'
+    const start = line.indexOf(startStr)
+    if (start !== -1) {
+      line = line.substring(start, line.length)
+      const endStr = '/comments'
+      const end = line.indexOf(endStr)
+      if (end !== -1) {
+        const id = line.substring(startStr.length, end)
+        console.log('daily challenge id found ->', id)
+
+        // POST the comments back!
+        return id
+      }
+    }
   })
-  .on('response', response => {
-    console.log('response!')
+  .then(id => {
+    if (typeof id === 'undefined') console.log('comment id not found')
+    return id
   })
-  .on('error', err => {
-    console.log(err)
-  })
-  .on('end', () => {
-    console.log('end!')
-    console.log('buffers', buffers.join(''))
+  .catch(err => console.log(err))
+
+getCommentId()
+  .then(id => {
+    let buffers = []
+    request
+      .post({
+        url: 'https://vtapi.voicetube.com/v2/core/zhTW/spkChallenges/1249/comments',
+        gzip: true,
+        rejectUnauthorized: false,
+        headers: {
+          'cache-control': 'no-cache',
+          'Accept': '*/*',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'User-Agent': 'yee'
+        }
+      })
+      .on('data', data => {
+        buffers.push(data.toString())
+      })
+      .on('response', response => {
+        console.log('response!')
+      })
+      .on('error', err => {
+        console.log(err)
+      })
+      .on('end', () => {
+        console.log('end!')
+        console.log(JSON.parse(buffers.join('')).length, 'comments received!')
+      })
   })
